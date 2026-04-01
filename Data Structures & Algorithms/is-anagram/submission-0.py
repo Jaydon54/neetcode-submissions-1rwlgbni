8d07cac3e0fd0f55aa4280885e7a658c30c9bf9d
Jaydon Christen <106935484+Jaydon54@users.noreply.github.com>
2026-04-01 14:28:12 +0100

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_list = list(s)
+        t_list = list(t)
+        if sorted(s_list) == sorted(t_list):
+            return True
+        return False
